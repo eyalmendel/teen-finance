3c010ab54fcd0e109f9e4852c1d56568fc357c83
Eyal Mendel <eyalmendel@gmail.com>
2024-12-15 22:22:03 +0200

    added more colors

--- app/config/colors.ts
+++ app/config/colors.ts
@@ -3,6 +3,9 @@ export const COLORS = {
     primary: '#39276f',
     secondary: '#9ce799',
     white: '#fff',
+    black: '#000',
     purple: '#e1e1fa',
     eggWhite: '#fdfdfd',
+    grey: '#909090',
+    lightPurple: '#e8def8',
 };

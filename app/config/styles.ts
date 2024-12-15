9920aa602dad1c1612d564bca04f4f5daccf99f9
Eyal Mendel <eyalmendel@gmail.com>
2024-12-15 22:19:20 +0200

    added global boxShadow style

--- app/config/styles.ts
+++ app/config/styles.ts
@@ -1,7 +1,14 @@
 import { StyleSheet } from 'react-native';
+import { COLORS } from './colors';
 
 export const STYLES = StyleSheet.create({
     rightAlignedText: {
         textAlign: 'right',
+    },
+    boxShadow: {
+        elevation: 4,
+        shadowColor: COLORS.primary,
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 10,
     },
 });

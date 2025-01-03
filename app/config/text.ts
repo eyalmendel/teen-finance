6f15df0cd0cbf77ec4268a87b24952a5a05770f5
Eyal Mendel <eyalmendel@gmail.com>
2025-01-03 11:18:12 +0200

    added more text styles

--- app/config/text.ts
+++ app/config/text.ts
@@ -3,12 +3,14 @@ export const TEXT = {
         headline: 48,
         mediumHeadline: 32,
         smallHeadline: 24,
+        medium: 18,
         default: 16,
         small: 12,
     },
     weight: {
         extraBold: 800,
         bold: 700,
+        semiBold: 600,
         regular: 400,
     },
 } as const;
